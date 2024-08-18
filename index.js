d63903a6a91360a0e06297cd7e38ab0e3dbc00d9
Erik <erik222333444555@gmail.com>
2024-08-18 20:45:56 +0200

    initial project

---
+++ index.js
@@ -0,0 +1,45 @@
+var swiper = new Swiper(".slider", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+		delay: 2600,
+		disableOnInteraction: true,
+	},
+  });
+
+  var swiper = new Swiper(".slider__two", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+		delay: 3000,
+		disableOnInteraction: true,
+	}
+  });
+
+  console.log("hello world!")
